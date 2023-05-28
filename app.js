6ec1c8eb3be3702b6d40bdd0eaa6bd02fd10f6ec
HimanshiGupta1656 <122423168+HimanshiGupta1656@users.noreply.github.com>
2023-05-28 15:30:10 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,103 @@
+const addBtn = document.querySelector("#addBtn")
+const main = document.querySelector("#main")
+addBtn.addEventListener(
+    "click",
+    function() {
+        addNote()
+    }
+)
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach(
+            (note) => {
+                data.push(note.value)
+            }
+        )
+        // console.log(data)
+    if (data.length === 0) {
+        localStorage.removeItem("notes")
+    } else {
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
+}
+//  <div class="note">
+// <div class="tool">
+//     <i class="fas fa-save"></i>
+//     <i class="fas fa-trash"></i>
+// </div>
+// <textarea></textarea>
+// </div>
+
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML = `
+    <div class="tool">
+         <i class="save fas fa-save"></i>
+         <i class="trash fas fa-trash"></i> 
+    </div>
+    <textarea>${text}</textarea>
+    `;
+
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function() {
+            note.remove()
+            saveNotes()
+        }
+    )
+    note.querySelector(".save").addEventListener(
+        "click",
+        function() {
+            saveNotes()
+        }
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function() {
+            saveNotes()
+        }
+    )
+    main.appendChild(note);
+    saveNotes()
+}
+
+
+(
+    function() {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        if (lsNotes === null) {
+            addNote()
+        } else {
+            lsNotes.forEach(
+                (lsNote) => {
+                    addNote(lsNote)
+                }
+            )
+        }
+
+    }
+)()
+const link = encodeURI(window.location.href);
+const msg = encodeURIComponent('Hey, I found this article');
+const title = encodeURIComponent('Article or Post Title Here');
+
+const fb = document.querySelector('.facebook');
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+
+const twitter = document.querySelector('.twitter');
+twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=javascript,programming`;
+
+const linkedIn = document.querySelector('.linkedin');
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
+
+const reddit = document.querySelector('.reddit');
+reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
+
+const whatsapp = document.querySelector('.whatsapp');
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${link}`;
+
+const telegram = document.querySelector('.telegram');
+telegram.href = `https://telegram.me/share/url?url=${link}&text=${msg}`;
